16b3c20e3fc30c31653cbe9b3935c05f91c873b5
Darren Rounce <drounce86@gmail.com>
2022-05-10 20:57:36 +0100

    created new and create methods for bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -6,9 +6,17 @@ class BookingsController < ApplicationController
   end
 
   def new
+    @booking = Booking.new
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    @booking.record = @record
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -18,5 +26,11 @@ class BookingsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:pick_up_date, :return_date, :record_id)
   end
 end
