fe56f5c2c14ff31db0f5942abf16bf12d2c34c5c
Edward Beesley <edwardjbeesley@gmail.com>
2022-05-10 19:29:07 +0100

    created seed file and added validation to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable , :registerable, and :omniauthable
-  devise :database_authenticatable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :records
   has_many :bookings
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "json"
+require "open-uri"
+require "ostruct"
+require "erb"
+
+def get_album(artist, album)
+  album = ERB::Util.url_encode(album)
+  artist = ERB::Util.url_encode(artist)
+  url = "https://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=2cea324142b7426923e60f8833987f00&artist=#{artist}&album=#{album}&format=json"
+  serialized = URI.open(url).read
+  album = JSON.parse(serialized, object_class: OpenStruct).album
+  create_record(album)
+end
+
+def create_record(album)
+  Record.create!(
+    title: album.name,
+    artist: album.artist,
+    genre: album.tags.tag[0].name,
+    year: Date.parse(lateralus.wiki.published).year,
+    available: true,
+    user_id: User.find(rand(1..User.count)),
+    price: 3
+  )
+  puts "Created Album #{album.name}!"
+end
+
+get_album('tool', 'lateralus')
+get_album('porcupine tree', 'deadwing')
+get_album('steven wilson', 'the future bites')
+get_album('Led Zeppelin', 'houses of the holy')
+get_album('bjork', 'debut')
+get_album('public enemy', 'fear of a black planet')
+get_album('kate bush', 'hounds of love')
+get_album('radiohead', 'ok computer')
+get_album('opeth', 'damnation')
+get_album('stevie wonder', 'songs in the key of life')
