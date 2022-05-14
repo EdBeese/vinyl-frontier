a86dcdea3212c00eb5835ded89a47f2ff654cfac
Edward Beesley <edwardjbeesley@gmail.com>
2022-05-14 11:45:39 +0100

    added attr_accessible to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable , :registerable, and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  attr_accessible :first_name, :last_name, :address_line_one, :address_line_two, :city, :postcode
+
   has_many :records, dependent: :destroy
   has_many :bookings, dependent: :destroy
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :address_line_one, presence: true
-  validates :address_line_two, presence: true
   validates :city, presence: true
   validates :postcode, presence: true
 end
