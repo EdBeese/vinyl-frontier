0df9018fb8dcbc625513582791cc5220b01ccab2
Edward Beesley <edwardjbeesley@gmail.com>
2022-05-14 12:44:01 +0100

    fixed signup fields

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,19 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def configure_permitted_parameters
-    # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :address_line_one, :address_line_two, :city, :postcode])
+  protected
 
-    # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :address_line_one, :address_line_two, :city, :postcode])
-  end
+          def configure_permitted_parameters
+               devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :address_line_one, :address_line_two, :city, :postcode, :email, :password, :password_confirmation)}
+
+               devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :address_line_one, :address_line_two, :city, :postcode, :email, :password, :current_password, :password_confirmation)}
+          end
+
+  # def configure_permitted_parameters
+  #   # For additional fields in app/views/devise/registrations/new.html.erb
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :address_line_one, :address_line_two, :city, :postcode])
+
+  #   # For additional in app/views/devise/registrations/edit.html.erb
+  #   devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :address_line_one, :address_line_two, :city, :postcode])
+  # end
 end
