7ca9f8df3d9e2d245924b561689a71a264037df9
Joe Williams-Langley <joewilliamslangley@gmail.com>
2022-05-14 11:01:36 +0100

    added env file and started booking index

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,6 +2,7 @@ class BookingsController < ApplicationController
   before_action :set_booking, only: [:show, :edit, :update, :destroy]
 
   def index
+    @bookings = Booking.where(user: current_user)
   end
 
   def show
