b86111f6d36c5be969b09e3db4ee6a5685a3e61b
Darren Rounce <drounce86@gmail.com>
2022-05-17 19:56:06 +0100

    added delete funmction to bookings show page

--- db/schema.rb
+++ db/schema.rb
@@ -65,7 +65,7 @@ ActiveRecord::Schema.define(version: 2022_05_16_200806) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "tracks"
-    t.text "about"
+    t.text "about", default: ""
     t.index ["user_id"], name: "index_records_on_user_id"
   end
 
