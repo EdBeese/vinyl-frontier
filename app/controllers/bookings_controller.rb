797925a169fa5af73f549221cf10c445084653e8
Edward Beesley <edwardjbeesley@gmail.com>
2022-05-19 20:46:53 +0100

    added menu tabs for bookings to see loans, borrows and active/ past

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,9 +3,13 @@ class BookingsController < ApplicationController
   before_action :set_record, only: [:new, :create]
 
   def index
-    @user = current_user
-    @bookings = Booking.all
     @records = Record.where(user: current_user)
+    @bookings_borrowing = Booking.where(user: current_user)
+    @bookings_loaning = Booking.all.select { |booking| booking.record.user.id == current_user.id }
+    @bookings_borrowing_current = @bookings_borrowing.select { |booking| booking.return_date > Date.today }
+    @bookings_borrowing_past = @bookings_borrowing.select { |booking| booking.return_date < Date.today }
+    @bookings_loaning_current = @bookings_loaning.select { |booking| booking.return_date > Date.today }
+    @bookings_loaning_past = @bookings_loaning.select { |booking| booking.return_date < Date.today }
   end
 
   def show
@@ -20,6 +24,7 @@ class BookingsController < ApplicationController
     @booking.record = @record
     @booking.user = current_user
     @booking.return_date = @booking.pick_up_date + 1.week
+    @record.toggle!(:available)
     if @booking.save
       redirect_to booking_path(@booking)
     else
