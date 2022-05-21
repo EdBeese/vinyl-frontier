e06d4ab87b711bef955cc25d5dc3f00b832464e5
Edward Beesley <edwardjbeesley@gmail.com>
2022-05-21 10:58:20 +0100

    added notification alerts when new booking made

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @records = Record.where(user: params[:id])
+    @new_messages = current_user.messages.where(read: false).count
     # @bookings_loaning = Booking.where(record: user = 2)
   end
 end
